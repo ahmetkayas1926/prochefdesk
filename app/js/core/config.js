49d9eff602d1938fb6c636607e8546e0c59324ac
ahmetkayas1926 <ahmetkaya.s1926@gmail.com>
2026-06-10 21:34:20 +0800

    2.43.12

--- app/js/core/config.js
+++ app/js/core/config.js
@@ -27,7 +27,7 @@ window.PCD_CONFIG = {
   // 3. APP DEFAULTS
   // ==============================================================
   APP_NAME: 'ProChefDesk',
-  APP_VERSION: '2.43.11',
+  APP_VERSION: '2.43.12',
   DEFAULT_CURRENCY: 'USD',
   DEFAULT_LOCALE: 'en',
 
